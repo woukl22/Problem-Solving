a36335488818e387b81cba66d0206debd6dc18f4
JaeUk Lee <69896250+woukl22@users.noreply.github.com>
2021-08-26 19:45:34 +0900

    Upload files

---
+++ step/02 - if문/01-1330-두 수 비교하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+  int A, B;
+  cin >> A >> B;
+
+  if(A > B){ cout << ">" << endl; }
+  else if(A < B){ cout << "<" << endl; }
+  else { cout << "==" << endl; }
+
+  return 0;
+}
---
+++ step/02 - if문/04-14681-사분면 고르기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+  int x, y;
+  cin >> x >> y;
+
+  if (x > 0) {
+    if (y > 0)
+      cout << "1" << endl;
+    else if (y < 0)
+      cout << "4" << endl;
+  } else if (x < 0) {
+    if (y > 0)
+      cout << "2" << endl;
+    else if (y < 0)
+      cout << "3" << endl;
+  }
+
+  return 0;
+}
