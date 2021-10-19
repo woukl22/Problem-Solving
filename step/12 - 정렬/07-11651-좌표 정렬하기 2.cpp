bac98fdf28dda53b6b6842ee9c4477d216b54af9
JaeUk Lee <69896250+woukl22@users.noreply.github.com>
2021-10-19 23:03:26 +0900

    Upload 07-11651-좌표 정렬하기 2.cpp

---
+++ step/12 - 정렬/07-11651-좌표 정렬하기 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char* argv[]){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  int N;
+  cin >> N;
+  vector<vector<int>> arr(N, vector<int>(2, 0));
+  
+  for(int i = 0; i < N; i++){
+    cin >> arr[i][1];
+    cin >> arr[i][0];
+  }
+
+  sort(arr.begin(), arr.end());
+
+  for(int i = 0; i < N; i++){
+    cout << arr[i][1] << " " << arr[i][0] << '\n';
+  }
+
+  return 0;
+}
